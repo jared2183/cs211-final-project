43aafdc885e38104b517017a7defad640f6c7aff
jared2183 <jaredmyang@gmail.com>
2021-11-23 12:00:19 -0600

    starting code

---
+++ final_project/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/jared/Downloads/final_project
+# Build directory: C:/Users/jared/Downloads/final_project/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/jared/Downloads/final_project/.cs211/cmake/211commands.cmake;90;add_test;C:/Users/jared/Downloads/final_project/CMakeLists.txt;21;add_test_program;C:/Users/jared/Downloads/final_project/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
